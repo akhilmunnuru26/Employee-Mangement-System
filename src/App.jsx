6515ab8f08b8fddbd39b3d8ccb28d05ccdc907e7
akhilmunnuru26 <munnuruakhil26@gmail.com>
2026-01-06 08:14:42 +0530

    fixed minor Routing Issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./routes/ProtectedRoute";
+import { useAuth } from "./context/useAuth";
 
 const App = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          isAuthenticated ? <Navigate to="/dashboard" replace /> : <Login />
+        }
+      />
+
+      
       <Route
         path="/dashboard"
         element={
@@ -14,7 +25,9 @@ const App = () => {
             <Dashboard />
           </ProtectedRoute>
         }
-      />     
+      />
+
+      {/* Catch-all */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
